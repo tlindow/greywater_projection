7f2f5849d88a0f0ceb566fa00003b4c21ee398ec
tlindow <tyler.lindow@gmail.com>
2018-11-05 13:41:08 -0800

    Reverted back to no button listening

--- public/line_drawing.js
+++ public/line_drawing.js
@@ -14,32 +14,32 @@ var max = 7000;
 var animeVar;
 // var thing;
 
-var play = function playAnimation() {
-  document.getElementById('lineDrawing').style.visibility = 'visible';
-  var pathEls = document.querySelectorAll('path');
-  for (var i = 0; i < pathEls.length; i++) {
-    var pathEl = pathEls[i];
-    var offset = anime.setDashoffset(pathEl);
-    pathEl.setAttribute('stroke-dashoffset', offset);
-    animeVar = anime({
-      targets: pathEl,
-      strokeDashoffset: [offset, 0],
-      duration: anime.random(min, max),
-      delay: anime.random(0, 3000),
-      loop: true,
-      direction: 'alternate',
-      easing: 'easeInOutSine',
-      autoplay: true
-    });
-  }
-};
-
-function checkFlag(thing, play) {
-  if (this.thing !== true) {
-    window.setTimeout(checkFlag, 100); /* this checks the flag every 100 milliseconds*/
-  } else {
-    this.play();
-  }
+// var play = function playAnimation() {
+document.getElementById('lineDrawing').style.visibility = 'visible';
+var pathEls = document.querySelectorAll('path');
+for (var i = 0; i < pathEls.length; i++) {
+  var pathEl = pathEls[i];
+  var offset = anime.setDashoffset(pathEl);
+  pathEl.setAttribute('stroke-dashoffset', offset);
+  animeVar = anime({
+    targets: pathEl,
+    strokeDashoffset: [offset, 0],
+    duration: anime.random(min, max),
+    delay: anime.random(0, 3000),
+    loop: true,
+    direction: 'alternate',
+    easing: 'easeInOutSine',
+    autoplay: true
+  });
 }
-
-checkFlag();
+// };
+//
+// function checkFlag(thing, play) {
+//   if (this.thing !== true) {
+//     window.setTimeout(checkFlag, 100); /* this checks the flag every 100 milliseconds*/
+//   } else {
+//     this.play();
+//   }
+// }
+//
+// checkFlag();
